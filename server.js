ff425a640479e5f1f0fa78d700c7e562c86f75b0
chriscuchetto <chriscuchetto@gmail.com>
2023-01-19 22:25:12 -0600

    for testing purposes

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, '/index.html'))
 );
 
 app.get('/notes', (req, res) =>
